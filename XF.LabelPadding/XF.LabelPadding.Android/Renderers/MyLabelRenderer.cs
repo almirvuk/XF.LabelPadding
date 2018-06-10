d5b77d4c1acd939b5439f6cc79c28907923cf9e5
Almir Vuk <almir.vuk@outlook.com>
2018-06-10 17:33:02 +0200

    Add project files.

---
+++ XF.LabelPadding/XF.LabelPadding.Android/Renderers/MyLabelRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XF.LabelPadding.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Label), typeof(MyLabelRenderer))]
+namespace XF.LabelPadding.Droid.Renderers
+{
+    public class MyLabelRenderer : LabelRenderer
+    {
+        public MyLabelRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            /*
+             * This is the main part of this renderers
+             * using this method we will
+             * not include font padding for 
+             * our Label controls. */
+            Control?.SetIncludeFontPadding(false);
+        }
+    }
+}
